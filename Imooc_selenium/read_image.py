b8580016593423cad2bd6b96dc4839ca0926bdbe
Administrator <1347525119@qq.com>
2021-07-03 14:19:30 +0800

    111

---
+++ Imooc_selenium/read_image.py
@@ -0,0 +1,23 @@
+#codinng=utf-8
+import pytesseract
+from PIL import Image
+import sys
+import os
+basepath=os.getcwd()
+sys.path.append(basepath)
+from ShowapiRequest import ShowapiRequest
+"打开图片"
+image = Image.open(basepath+"\\Image\\image2.png")
+"通过pytesseract对图片识别，返回一个字符串"
+text = pytesseract.image_to_string(image)
+print(text)
+
+"调用第三方接口，传入图片地址，返回一个字符串"
+r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
+r.addBodyPara("typeId", "35")
+r.addBodyPara("convert_to_jpg", "0")
+image = basepath+"\\Image\\image2.png"
+r.addFilePara("image", r"image") #文件上传时设置
+res = r.post()
+text = res.json()['showapi_res_body']['Result']
+print(text) # 返回信息
--- Imooc_selenium/start_browser.py
+++ Imooc_selenium/start_browser.py
@@ -1,14 +1,58 @@
 # coding=utf-8
+import sys
+import os
+basepath=os.getcwd()
+sys.path.append(basepath)
 from selenium import webdriver
 import time
+import random
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from PIL import Image
 
 driver = webdriver.Chrome()
+
 driver.maximize_window()
 driver.get("http://www.5itest.cn/register")
 time.sleep(3)
 print(EC.title_contains("注册"))
-driver.find_element_by_id("register_email").send_keys("jiangfeng_94@163.com")
+
+
+"判断元素是否可见，即元素等待方式"
+element = driver.find_elements_by_class_name("controls")
+"by方法包内，显示等待，传入定位方式及元素"
+locator = (By.CLASS_NAME, "controls")
+"传入dirver，等待时间，以及元素位置"
+WebDriverWait(driver,1).until(EC.visibility_of_element_located(locator))
+
+"生成随机字符串,random返回的是list，所以要join一下，转成字符串"
+user_email = ''.join(random.sample('1234567890abcdefg',8))+"@163.com"
+print(user_email)
+
+email_name = driver.find_element_by_id("register_email")
+email_name.send_keys("jiangfeng_94@163.com")
+"selenium保存图片"
+driver.save_screenshot(basepath+"\\Image\\image.png")
+"获取验证码元素位置"
+code_element = driver.find_element_by_id("getcode_num")
+print(code_element.location)#{"x":123,"y":345}
+"通过元素的左上角坐标，算出其他几个坐标点"
+left = code_element.location['x']
+top = code_element.location['y']
+right = code_element.size['width']+left
+height = code_element.size['height']+top
+im = Image.open(basepath+"\\Image\\image.png")
+"根据坐标裁剪出验证码图片"
+img = im.crop((left,top,right,height))
+"保存验证码图片"
+img.save(basepath+"\\Image\\image2.png")
+
+
+
+
+"获取此时输入框的值"
+print(email_name.get_attribute("value"))
 
 "先查找父节点，查找出父节点后再查出父节点下的子节点"
 user_name_element_node = driver.find_elements_by_class_name("controls")[1]
@@ -18,3 +62,4 @@ user_element.send_keys("saa1231saf")
 
 driver.find_element_by_name("password").send_keys("sahfkhj123123")
 driver.find_element_by_xpath("//*[@id='captcha_code']").send_keys("111111")
+driver.close()
