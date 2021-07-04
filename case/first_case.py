6d9de1e38f381f2355dca9760a62845814ff7fbf
Administrator <1347525119@qq.com>
2021-07-04 14:25:54 +0800

    22312ss

--- case/first_case.py
+++ case/first_case.py
@@ -14,9 +14,6 @@ class FirstCase(object):
         self.login=RregisterBusiness(driver)
         # 邮箱、用户名、密码、验证码、错误信息定位元素、错误提示信息
 
-    def test_login_email_error(self):
-        email_error = self.login.login_email_error('1314@qq.com', 'user1111@qq.com', '111111','1231')
-        return self.assertFalse(email_error, "测试失败")
 
     def test_login_username_error(self):
         username_error = self.login.login_name_error('12123@qq.com', 't1', '111111','1231')
@@ -37,10 +34,13 @@ class FirstCase(object):
 
 def main():
         first = FirstCase()
-        first.test_login_code_error()
         first.test_login_email_error()
         first.test_login_password_error()
         first.test_login_username_error()
+
+        first.test_login_code_error()
+
+
         first.test_login_success()
 
 if __name__ == '__main__':
